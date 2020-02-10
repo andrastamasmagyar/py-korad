f3911b5646cb39b041c08c6852c01225681dd8b6
andrastamasmagyar <60848243+andrastamasmagyar@users.noreply.github.com>
2020-02-10 21:58:42 +0100

    Update __init__.py

---
+++ korad/__init__.py
@@ -0,0 +1,18 @@
+Hi Mango-kid,
+
+I am writing you here because I did not find any of your contact. 
+I am trying to do a connection with my Korad KEL103 but it does not communicate.
+In phycharm it gives this error:
+    data, server = self.sock.recvfrom(1024)
+socket.timeout: timed out
+
+This is my setup connection parameters: kel = KEL103.kel103("192.168.1.181", "192.168.8.128", 18190)
+
+What could be the problem, what do you think?
+
+Please help me!
+
+My email is: magyar.andras.tamas@outlook.com
+
+Thank you,
+Andras Tamas Magyar
